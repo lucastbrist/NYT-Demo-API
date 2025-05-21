ce3c0f850e157750be604465adf2de5ac390d25d
Lucas Brist <lucastbrist@gmail.com>
2025-05-21 16:47:15 -0500

    first commit

---
+++ src/main/java/com/ltb/api_demo/services/ArticleService.java
@@ -0,0 +1,35 @@
+package com.ltb.api_demo.services;
+
+import com.ltb.api_demo.models.Article;
+import com.ltb.api_demo.models.NytResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    @Value("${api_key}")
+    private String apikey;
+
+    @Value("${mostPopularUrl}")
+    private String mostPopularUrl;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public List<Article> getMostPopular() {
+        NytResponse response = restTemplate.getForObject(
+                mostPopularUrl + "api-key=" + apikey, NytResponse.class);
+        List<Article> results = new ArrayList<>();
+        if (response != null && response.getStatus().equals("OK")) {
+            return response.getResults();
+        } else {
+            return results;
+        }
+    }
+}
